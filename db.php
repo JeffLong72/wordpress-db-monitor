0131626db16ab941954b43f7892a63fa92bbde14
JeffLong72 <45916412+JeffLong72@users.noreply.github.com>
2019-05-07 08:44:24 +0200

    Add files via upload

---
+++ db.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ Plugin Name: Extended wpdb
+ Description: A few extra functions for wpdb (eg. log mysql queries, send email if database goes down )
+ Version: 	  1.0
+ Author:	  ACC Digital (Jeff Long)
+ Author URI:  https://www.acc.cc/
+*/
+ 
+class wpdb_extended extends wpdb {
+	
+	// the email where we want to send reports too
+	private $email_recipient = "jeff.long@acc.cc";
+	
+	public function __construct()
+	{
+		parent::__construct( DB_USER, DB_PASSWORD, DB_NAME, DB_HOST );
+	}
+ 
+	public function sendEmail($db) 
+	{
+		// set subject
+		$subject = 'MySQL database offline';
+		
+		// set db error message
+		$message = print_r($db->errors, true);
+
+		// include the contents of mysql log file in message 
+		// ( requires wp-log-mysql plugin installed & active )
+		if(file_exists(dirname(__FILE__).'/plugins/wp-log-mysql/sql_log.html')) 
+		{
+			$message.= "<h2>Last logged MySQL queries</h2>";
+			$message.= file_get_contents(dirname(__FILE__).'/plugins/wp-log-mysql/sql_log.html');
+		}
+		
+		// set headers
+		$headers = "From: ".$this->email_recipient."\r\n";
+		$headers.= "Reply-To: ".$this->email_recipient."\r\n";
+		$headers.= "MIME-Version: 1.0\r\n";
+		$headers.= "Content-Type: text/html; charset=UTF-8\r\n";
+		
+		// send email
+		mail($this->email_recipient, $subject, $message, $headers);
+	}
+}
+
+// get wp db
+global $wpdb;
+
+// create our extended db object
+$wpdb = new wpdb_extended();
+
+// if we have a db error, we need to send an email
+$db_error = (!empty($wpdb->error)) ? $wpdb->sendEmail($wpdb->error) : "";
+
---
+++ wp-log-mysql/wp-log-mysql.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ Plugin Name: WP Log MySQL
+ Description: Saves the last MySQL Queries that ran to log file.
+ Version:     1.0.0
+ Author:      ACC Digital (Jeff Long)
+ Author URI:  https://www.acc.cc/
+ */
+ 
+function sql_logger() {
+	
+	// get wp db connection
+    global $wpdb;
+	
+	// set log file we want to write too
+	// ( note: use "w" not "a", as we only want the last mysql queries, 
+	//   - we want to overwrite the log file to save on file size )
+    $log_file = fopen(dirname(__FILE__).'/sql_log.html', 'w');
+	
+	// write date to log file
+    fwrite($log_file, date("F j, Y, g:i:s a")."<br /><br />");
+	
+	// for each mysql query, write to log file
+    foreach($wpdb->queries as $q) {
+        fwrite($log_file, $q[0] . " - ($q[1] seconds)" . "<br /><br />");
+    }
+	
+	// close file
+    fclose($log_file);
+}
+
+// add the log entry just before PHP shuts down execution
+// ( ref: https://codex.wordpress.org/Plugin_API/Action_Reference/shutdown )
+add_action('shutdown', 'sql_logger');
